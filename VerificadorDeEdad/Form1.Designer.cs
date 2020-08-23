57025c81ca5811ee72dd39346e456aa792d752de
Ishwar Lopez Rua <ilopezr101@gmail.com>
2020-08-23 15:55:50 -0500

    Programa que verifica si se es mayor de edad segun la edad ingresada

---
+++ VerificadorDeEdad/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace VerificadorDeEdad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtedad = new System.Windows.Forms.TextBox();
+            this.lblRespuesta = new System.Windows.Forms.Label();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.btnLimpiarCampo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(214, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(239, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Verificar edad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(249, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese edad";
+            // 
+            // txtedad
+            // 
+            this.txtedad.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtedad.Location = new System.Drawing.Point(284, 184);
+            this.txtedad.Name = "txtedad";
+            this.txtedad.Size = new System.Drawing.Size(88, 44);
+            this.txtedad.TabIndex = 2;
+            this.txtedad.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtedad.TextChanged += new System.EventHandler(this.txtedad_TextChanged);
+            // 
+            // lblRespuesta
+            // 
+            this.lblRespuesta.AutoSize = true;
+            this.lblRespuesta.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRespuesta.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblRespuesta.Location = new System.Drawing.Point(210, 276);
+            this.lblRespuesta.Name = "lblRespuesta";
+            this.lblRespuesta.Size = new System.Drawing.Size(111, 24);
+            this.lblRespuesta.TabIndex = 3;
+            this.lblRespuesta.Text = "Respuesta";
+            this.lblRespuesta.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblRespuesta.Visible = false;
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnVerificar.FlatAppearance.BorderSize = 4;
+            this.btnVerificar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnVerificar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerificar.Location = new System.Drawing.Point(214, 350);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(105, 48);
+            this.btnVerificar.TabIndex = 4;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnLimpiarCampo
+            // 
+            this.btnLimpiarCampo.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnLimpiarCampo.FlatAppearance.BorderSize = 4;
+            this.btnLimpiarCampo.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnLimpiarCampo.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnLimpiarCampo.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiarCampo.Location = new System.Drawing.Point(348, 350);
+            this.btnLimpiarCampo.Name = "btnLimpiarCampo";
+            this.btnLimpiarCampo.Size = new System.Drawing.Size(105, 48);
+            this.btnLimpiarCampo.TabIndex = 5;
+            this.btnLimpiarCampo.Text = "limpiar campo";
+            this.btnLimpiarCampo.UseVisualStyleBackColor = true;
+            this.btnLimpiarCampo.Click += new System.EventHandler(this.btnLimpiarCampo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(651, 450);
+            this.Controls.Add(this.btnLimpiarCampo);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.lblRespuesta);
+            this.Controls.Add(this.txtedad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtedad;
+        private System.Windows.Forms.Label lblRespuesta;
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.Button btnLimpiarCampo;
+    }
+}
+
---
+++ VerificadorDeEdad/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VerificadorDeEdad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtedad_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double Edad;
+
+            Edad = double.Parse(txtedad.Text);
+
+
+            if (Edad >=18) {
+                lblRespuesta.Text = " Usted es mayor de edad ";
+                
+
+            }
+            else if(Edad <18 ) {
+                lblRespuesta.Text = "Usted es menor de edad !!!";
+                
+
+            }
+            lblRespuesta.Visible = true;
+        }
+
+        private void btnLimpiarCampo_Click(object sender, EventArgs e)
+        {
+            txtedad.Text = String.Empty;
+            lblRespuesta.Text = String.Empty;
+            txtedad.Focus();
+        }
+    }
+    }
+
